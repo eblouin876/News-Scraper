bd2191f8c38746536a308d34f0af46e064b4af37
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-04-22 20:33:27 -0700

    Basic working model with dummy data. Need to build the scraper

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+function updateComments() {
+  $.get("/api/comments", data => {
+    console.log(data);
+  });
+}
+
+$(document).ready(() => {
+  $(".collapsible").collapsible();
+
+  $(".submit-comment").on("click", function() {
+    let article = $(this).attr("data-article");
+    let commentId = $(this).attr("data-input-id");
+    let comment = $(`#${commentId}`).val();
+    let post = { article: article, comment: comment };
+    $.post("/api/comment", post).then(() => {
+      window.location = window.location;
+    });
+    $(`#${commentId}`).val("");
+  });
+});
--- routes.js
+++ routes.js
@@ -1,32 +1,72 @@
 let Articles = require("./articlesModel.js");
 let scraper = require("./scraper.js");
+let log = require("con-logger");
 
-let test = {
+let homeTest = {
   articles: [
     {
-      title: "test",
+      title: "test3",
       url: "https://www.google.com",
       body: "This is the body of the article"
     },
     {
-      title: "test",
+      title: "test4",
       url: "https://www.google.com",
       body: "This is the body of the article"
     }
   ]
 };
+
+let savedTest = {
+  articles: [
+    {
+      title: "test1",
+      url: "https://www.google.com",
+      body: "This is the body of the article",
+      comments: [
+        { comment: "This", user: "Me" },
+        { comment: "that", user: "You" },
+        { comment: "the other", user: "They" }
+      ]
+    },
+    {
+      title: "test2",
+      url: "https://www.google.com",
+      body: "This is the body of the article",
+      comments: [
+        { comment: "This", user: "Me" },
+        { comment: "that", user: "You" },
+        { comment: "the other", user: "They" }
+      ]
+    }
+  ]
+};
+
 module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.render("index", homeTest);
+  });
+
+  app.get("/saved", function(req, res) {
+    //   Query the database and get the saved articles
+    res.render("saved", savedTest);
+  });
+
+  app.put("/api/clear", function(req, res) {
+    //   Clear the saved data in the database
+
+    res.redirect("/saved");
+  });
+
   app.post("/api/newScrape", function(req, res) {
     // Run the scrapper which will return the necessary object
     // Store in the database
     // Redirect to home and render with new data
   });
 
-  app.get("/", function(req, res) {
-    res.render("index", test);
-  });
-
-  app.get("/saved", function(req, res) {
-    res.render("saved", test);
+  app.post("/api/comment", function(req, res) {
+    log(req.body);
+    // Do all of the logic here
+    res.send("Added comment");
   });
 };
