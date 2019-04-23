b79353a9d5a27e15ea14cce84d81bd5283d8b31b
Emile Blouin <emile_blouin@alumni.brown.edu>
2019-04-22 19:08:26 -0700

    Basic building in handlebars

--- routes.js
+++ routes.js
@@ -1,6 +1,20 @@
 let Articles = require("./articlesModel.js");
 let scraper = require("./scraper.js");
 
+let test = {
+  articles: [
+    {
+      title: "test",
+      url: "https://www.google.com",
+      body: "This is the body of the article"
+    },
+    {
+      title: "test",
+      url: "https://www.google.com",
+      body: "This is the body of the article"
+    }
+  ]
+};
 module.exports = function(app) {
   app.post("/api/newScrape", function(req, res) {
     // Run the scrapper which will return the necessary object
@@ -9,10 +23,10 @@ module.exports = function(app) {
   });
 
   app.get("/", function(req, res) {
-    res.render("index");
+    res.render("index", test);
   });
 
   app.get("/saved", function(req, res) {
-    res.render("saved");
+    res.render("saved", test);
   });
 };
